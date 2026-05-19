974bc6771a3091d4af433e30a431d36d495cc27a
antoniocosta0 <antonio.lc119@gmail.com>
2026-05-18 22:59:29 -0300

    feat: adiciona esqueleto das classes Gym e PremiumStudent

--- model/Gym.java
+++ model/Gym.java
@@ -1,15 +1,5 @@
 package model;
 
 public class Gym <acad> extends Student {
-    private String name;
-    private String address;
-    private String telephone;
-    private String email;
 
-    private Gym(String name, String address, String telephone, String email) {
-        this.name = name;
-        this.address = address;
-        this.telephone = telephone;
-        this.email = email;
-    }
 }
--- model/PremiumStudent.java
+++ model/PremiumStudent.java
@@ -1,5 +1,5 @@
 package model;
 
-public class PremiumStudent {
+public class PremiumStudent extends Student {
     
 }
